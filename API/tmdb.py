82d21cf9f7e5fae01609ea38908ebda86b9eeb49
Gustavo S. J <95196272+TyrionLnnstr@users.noreply.github.com>
2024-01-06 17:35:13 -0300

    ✨ FEAT(tmdb) : get api and save in json file

---
+++ API/tmdb.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import os 
+from dotenv import load_dotenv 
+
+
+'''
+Requests  - consultar APIS
+OS - consultar dados do Sistema Operacional ** DOTENV e o nosso arquivo especial para guardar os segredos.
+Json - manipulacao de arquivos .json
+'''
+
+
+load_dotenv() 
+Authorization = os.getenv("tmdb_Authorization")
+
+
+'''
+Neste caso em questão foi importado o objeto Authorization com segredo para conexao a API
+'''
+
+
+include_adult = "false"
+include_video = "false"
+language = "pt-BR"
+page = "1"
+
+parametros = f"include_adult={include_adult}&include_video{include_video}&language={language}&page{page}"
+
+url = f"https://api.themoviedb.org/3/discover/movie?{parametros}"
+headers = {"accept": "application/json","Authorization":Authorization}
+response = requests.get(url, headers=headers)
+
+
+'''
+neste trecho nos colocamos no codigo qual a url no qual vamos fazer o get
+por ser uma API privada necessitamos informar o segredo dentro do cabecalho
+por seguranca foi utilizado o arquivo .env para acessar o segredo
+'''
+
+
+data = response.json()
+file = open("mentoria(eng.dados)/data/tmdb.json", "w", encoding=("utf-8")) 
+json.dump(data, file, ensure_ascii=False, indent=2)
+
+
+'''
+response.json acessa o get e o transforma em json
+---------------------------------------------------
+open foi utilizado para criar nosso arquivo como 
+nome "tmdb.json"
+modo de escrita "w" - caso existam dados eles serão sobrescrito
+codificacao "utf-8"
+---------------------------------------------------
+dump utilizamos o ensure_ascii = false 
+isso faz com que os caracteres especiais como acentos sejam exibidos no arquivo
+caso contrario teremos resultados como este fam\u00edlia  onde \u00ed representa i com acento
+'''
