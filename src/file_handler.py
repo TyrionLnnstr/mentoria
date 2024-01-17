85f84513b810d2666d47028c3af0fd6b77f4922e
Gustavo S. J <95196272+TyrionLnnstr@users.noreply.github.com>
2024-01-17 18:51:33 -0300

    ➖ REMOVE(FILE_HANDLER): CURRENT_DATE AND DEPENDENCE TMDB_API

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,13 +1,9 @@
-import  json
-import  datetime
-import  tmdb_api
-from    pathlib     import Path
-
-current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+import json
+from pathlib import Path
 
 class FileHandler:
-    def save_data(self, data, current_datetime, page):
-        self.file_name = f"tmdb_{current_datetime}_page{tmdb_api.pag}.json" 
-        self.caminho = Path("mentoria(eng.dados)") / "data" / self.file_name
+    def save_data(self, data, date, page):
+        self.file_name = f"tmdb_{date}_page{page}.json" 
+        self.caminho = Path('extract_tmdb','data','{self.file_name}')
         with open(self.file_name, "w", encoding="utf-8") as file:
-            json.dump(tmdb_api.dados, file, ensure_ascii=False, indent=2)
+            json.dump(data, file, ensure_ascii=False, indent=2)
