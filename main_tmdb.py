c8485ec120c100b0d276785c92e00e2833573ada
Gustavo S. J <95196272+TyrionLnnstr@users.noreply.github.com>
2024-01-17 18:48:34 -0300

    🔨 UPDATE(MAIN) :  IF __NAME__

---
+++ main_tmdb.py
@@ -0,0 +1,17 @@
+from extract_tmdb.src.tmdb_api import TMDB
+from extract_tmdb.src.file_handler import FileHandler
+import time
+
+if  __name__ == "__main__":
+    
+    file_handler  = FileHandler()
+    tmdb_instance = TMDB()
+    tmdb_instance.get_api()
+    data =  tmdb_instance.data()
+    
+    while True:
+        file_handler.save_data(data, tmdb_instance.dt_time(), data['page'])
+        if data['page'] >= data['total_pages']:
+            break
+        time.sleep(1)
+        data['page'] += 1
