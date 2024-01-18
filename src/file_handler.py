6a2ef5f567e8194329dd0791a4fe807e4c566d06
Gustavo S. J <95196272+TyrionLnnstr@users.noreply.github.com>
2024-01-17 23:03:06 -0300

    🐛 BUG:(FILE_HANDLER): OPEN self.file_name for self.caminho

--- src/file_handler.py
+++ src/file_handler.py
@@ -4,6 +4,6 @@ from pathlib import Path
 class FileHandler:
     def save_data(self, data, date, page):
         self.file_name = f"tmdb_{date}_page{page}.json" 
-        self.caminho = Path('extract_tmdb','data','{self.file_name}')
-        with open(self.file_name, "w", encoding="utf-8") as file:
+        self.caminho = Path('extract_tmdb','data', self.file_name)
+        with open(self.caminho, "w", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=2)
